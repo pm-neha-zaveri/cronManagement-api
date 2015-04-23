37c1c7f60746e54dac020d0fdf8e1de2561cb175
pm-neha-zaveri <neha.zaveri@pubmatic.com>
2015-04-23 11:18:06 +0530

    Added new changes

--- cronManagement-api/cronmanagement/src/main/java/cronmanagement/services/impl/CronDetailsServiceImpl.java
+++ cronManagement-api/cronmanagement/src/main/java/cronmanagement/services/impl/CronDetailsServiceImpl.java
@@ -2,18 +2,15 @@ package cronmanagement.services.impl;
 
 
 import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Service;
 import org.springframework.context.MessageSource;
-import javax.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.pubmatic.apiutils.bean.PageSupporter;
-import com.pubmatic.apiutils.bean.RequestDetails;
-import com.pubmatic.apiutils.bean.PageSupporter;
-import com.pubmatic.apiutils.exception.DataValidationException;
-import com.pubmatic.apiutils.exception.ServiceException;
-import cronmanagement.bean.CronManagement;
+import org.springframework.stereotype.Service;
+
+import cronmanagement.bean.CronDetails;
 import cronmanagement.services.CronDetailsService;
 
 
@@ -25,38 +22,9 @@ public class CronDetailsServiceImpl implements CronDetailsService{
     @Resource
     MessageSource  cronmanagementMessageSource;   
     
-    @Override
-    public CronManagement get(Long userId, Long idToGet) throws ServiceException, DataValidationException {
-        // TODO Auto-generated method stub	
-	CronManagement changeObjectName = new CronManagement();
-        changeObjectName.setName("Demo API");
-        changeObjectName.setId(1L);
-
-        return changeObjectName;
-    }
-
-    @Override
-    public PageSupporter<CronManagement> search(Long userId, RequestDetails requestDetails) throws ServiceException,
-            DataValidationException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public CronManagement create(Long userId, CronManagement entityToCreate) throws ServiceException, DataValidationException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public CronManagement update(Long userId, CronManagement entityToUpdate) throws ServiceException, DataValidationException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Boolean delete(Long userId, Long entityIdToDel) throws ServiceException, DataValidationException {
-        // TODO Auto-generated method stub
-        return null;
-    } 
+	@Override
+	public List<CronDetails> getCronDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	} 
 }
