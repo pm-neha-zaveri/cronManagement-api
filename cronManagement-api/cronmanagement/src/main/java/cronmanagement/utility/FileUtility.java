4ccdfb5965b206fba1f45e5e09514b2907da8fee
pm-neha-zaveri <neha.zaveri@pubmatic.com>
2015-04-24 12:38:18 +0530

    Changes for reading from cronFile

--- cronManagement-api/cronmanagement/src/main/java/cronmanagement/utility/FileUtility.java
+++ cronManagement-api/cronmanagement/src/main/java/cronmanagement/utility/FileUtility.java
@@ -1,27 +1,52 @@
 package cronmanagement.utility;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.Calendar;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import com.pubmatic.castrum.exceptions.APIException;
 
 public class FileUtility {
-    
-    public final static Log LOGGER = LogFactory.getLog(FileUtility.class);
-    
-    public static void renameFile(String cronjobfilepath) {
-        File oldfile = new File(cronjobfilepath);
-        String newFileName = Calendar.getInstance().getTimeInMillis()
-                + cronjobfilepath;
-        File newfile = new File(newFileName);
 
-        if (oldfile.renameTo(newfile)) {
-            LOGGER.info("Rename succesful"+cronjobfilepath +" to "+newFileName);
-            oldfile.delete();
-        } else {
-            LOGGER.info("Rename failed");
-        }
-    }
+	public final static Log LOGGER = LogFactory.getLog(FileUtility.class);
+
+	private static Properties cronManangementProperties = null;
+
+	public static void renameFile(String cronjobfilepath) {
+		File oldfile = new File(cronjobfilepath);
+		String newFileName = Calendar.getInstance().getTimeInMillis()
+				+ cronjobfilepath;
+		File newfile = new File(newFileName);
+
+		if (oldfile.renameTo(newfile)) {
+			LOGGER.info("Rename succesful" + cronjobfilepath + " to "
+					+ newFileName);
+			oldfile.delete();
+		} else {
+			LOGGER.info("Rename failed");
+		}
+	}
+
+	public static String getPropertyValue(String propKey) {
+		// Load the properties file only once
+		try {
+			if (cronManangementProperties == null) {
+				cronManangementProperties = new Properties();
+				InputStream inputStream = new ClassPathResource(
+						"cronManagement.properties").getInputStream();
+				cronManangementProperties.load(inputStream);
+				inputStream.close();
+			}
+		} catch (Exception exp) {
+			LOGGER.error("Error in loading " + "cronManagement.properties");
+			exp.printStackTrace();
+		}
+		return cronManangementProperties.getProperty(propKey);
+	}
 
 }
