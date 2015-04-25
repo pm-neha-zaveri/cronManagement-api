86febb2fd361963b046320dac48d460b330b81ee
pm-raghunandan-gupta <raghunandan.gupta@pubmatic.com>
2015-04-25 16:18:25 +0530

    null pointer exception fixed.

--- cronManagement-api/cronmanagement/src/main/java/cronmanagement/schedulers/CronHealthUpdateSchedulerTask.java
+++ cronManagement-api/cronmanagement/src/main/java/cronmanagement/schedulers/CronHealthUpdateSchedulerTask.java
@@ -1,5 +1,7 @@
 package cronmanagement.schedulers;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,10 +10,13 @@ import cronmanagement.services.HealthCheckService;
 @Service
 public class CronHealthUpdateSchedulerTask {
 
+    public final static Log LOGGER = LogFactory.getLog(CronHealthUpdateSchedulerTask.class);
+    
     @Autowired
     HealthCheckService healthCheckService;
     
     public void updateHealthCheckData(){
+        LOGGER.info("Within " + getClass().getName() + " updateHealthCheckData method.");
         healthCheckService.calculateHealth();
     }
     
