1cb318fa6ade59a5c2deff15f912b7156913a6cd
pm-raghunandan-gupta <raghunandan.gupta@pubmatic.com>
2015-04-26 15:13:30 +0530

    Updated query commiited.

--- cronManagement-api/cronmanagement/app-resources/tables.sql
+++ cronManagement-api/cronmanagement/app-resources/tables.sql
@@ -79,3 +79,7 @@ alter table cronjob modify column noOfRuns int(10) UNSIGNED NOT NULL Default 0;
 alter table cronjob modify column noOfAlerts int(10) UNSIGNED NOT NULL;
 
 ALTER TABLE `cronmanagement`.`cronjob` CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT  ;
+
+insert into cron_alert(cron_id,server_id,start_time,end_time,run_time,threshold) select cron_id,server_id,start_time,end_time,run_time,threshold from cron_log_history where cron_id=1 and threshold >= run_time;
+update cron_alert ca set ca.server_id=(select distinct server_id from cronjob where id=ca.cron_id);
+update cron_alert ca set ca.dc_id=(select distinct dc_id from server where id=ca.server_id);
